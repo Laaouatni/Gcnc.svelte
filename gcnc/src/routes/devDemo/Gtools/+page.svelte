0c3c0fcc32dc6945d789fd52b5eca2e0fe37fd8b
laaouatni <laaouatni.anas@outlook.it>
2023-03-27 00:19:46 +0200

    added open/closing of bottomNav when at least one tool is checked

--- gcnc/src/routes/devDemo/Gtools/+page.svelte
+++ gcnc/src/routes/devDemo/Gtools/+page.svelte
@@ -2,12 +2,17 @@
   import GtoolsWarehouse from "$lib/Gcore/Gtools/GtoolsWarehouse/GtoolsWarehouse.svelte";
   import GuiTitle from "$lib/GuiComponents/GuiTitle/GuiTitle.svelte";
   import { onMount } from "svelte";
+  import { scale, slide } from "svelte/transition";
+
+  import GtoolsStore from "../../../database/Gtools/GtoolsStore";
+
+  $: isAtLeastOneChecked = $GtoolsStore.some((obj) => obj.isEditChecked);
 
   let GtoolsPageContainer: HTMLElement;
-  
+
   onMount(() => {
-    GtoolsPageContainer.parentElement.style.padding = 0;
-  })
+    GtoolsPageContainer!.parentElement!.style.padding = "0px";
+  });
 </script>
 
 <div class="flex flex-col h-full" bind:this={GtoolsPageContainer}>
@@ -15,14 +20,14 @@
     <GuiTitle Gtitle="tools" />
     <GtoolsWarehouse />
   </div>
-  
-  <div
-    class="flex bg-white w-full p-4"
-  >
-    <button
-      class="flex-1 bg-red-500 shadow-lg shadow-red-500 rounded-lg p-4 text-white font-semibold uppercase"
-      >delete</button
-    >
-  </div>
-</div>
 
+  {#if isAtLeastOneChecked}
+    <div class="flex bg-white w-full p-4" transition:scale>
+      <button
+        transition:slide
+        class="flex-1 bg-red-500 shadow-lg shadow-red-500 rounded-lg p-4 text-white font-semibold uppercase"
+        >delete</button
+      >
+    </div>
+  {/if}
+</div>
