899fc61aa4fb2fb7a3f16dbbe9d873adc61950c8
laaouatni <laaouatni.anas@outlook.it>
2023-03-26 23:56:10 +0200

    now Gtools page have a bottomNav using flexbox (functionality need to be added then, for now the U for one button example is completed)

--- gcnc/src/routes/devDemo/Gtools/+page.svelte
+++ gcnc/src/routes/devDemo/Gtools/+page.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
   import GtoolsWarehouse from "$lib/Gcore/Gtools/GtoolsWarehouse/GtoolsWarehouse.svelte";
   import GuiTitle from "$lib/GuiComponents/GuiTitle/GuiTitle.svelte";
+  import { onMount } from "svelte";
+
+  let GtoolsPageContainer: HTMLElement;
+  
+  onMount(() => {
+    GtoolsPageContainer.parentElement.style.padding = 0;
+  })
 </script>
 
-<GuiTitle Gtitle="tools" />
+<div class="flex flex-col h-full" bind:this={GtoolsPageContainer}>
+  <div class="overflow-auto p-4">
+    <GuiTitle Gtitle="tools" />
+    <GtoolsWarehouse />
+  </div>
+  
+  <div
+    class="flex bg-white w-full p-4"
+  >
+    <button
+      class="flex-1 bg-red-500 shadow-lg shadow-red-500 rounded-lg p-4 text-white font-semibold uppercase"
+      >delete</button
+    >
+  </div>
+</div>
 
-<GtoolsWarehouse />
