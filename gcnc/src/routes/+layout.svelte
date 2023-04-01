dd154497616b58450371a0cf6754957e879205cd
laaouatni <laaouatni.anas@outlook.it>
2023-04-01 15:35:17 +0200

    other mini changes

--- gcnc/src/routes/+layout.svelte
+++ gcnc/src/routes/+layout.svelte
@@ -1,46 +1,5 @@
-<script lang="ts">
+<script>
   import "../app.css";
-  import { page } from "$app/stores";
-  import { slide } from "svelte/transition";
-
-  import GuiPageNav from "$lib/GuiComponents/GuiPageNav/GuiPageNav.svelte";
-
-  class Gpage {
-    GpageName: string;
-    GpageRoute: string;
-    GpagePrefix: string;
-
-    constructor(GpageName: string) {
-      this.GpageName = GpageName;
-      this.GpagePrefix = "devDemo";
-      this.GpageRoute = `/${this.GpagePrefix}/${this.GpageName}`;
-    }
-  }
-
-  const navigationArray = [
-    new Gpage("G1"),
-    new Gpage("Gtools"),
-    new Gpage("Gmachine"),
-  ];
-
-  let navHeight: number;
 </script>
 
-<GuiPageNav GpagesArray={navigationArray} bind:navHeight />
-
-{#key $page.route}
-  <main
-    in:slide
-    class="fixed w-full overflow-auto p-4"
-    style="--nav-height: {navHeight}px;"
-  >
-    <slot />
-  </main>
-{/key}
-
-<style>
-  main {
-    top: var(--nav-height);
-    height: calc(100vh - var(--nav-height));
-  }
-</style>
+<slot />
--- gcnc/src/routes/devDemo/Gtools/+page.svelte
+++ gcnc/src/routes/devDemo/Gtools/+page.svelte
@@ -1,33 +1,22 @@
 <script lang="ts">
-  import GtoolsWarehouse from "$lib/Gcore/Gtools/GtoolsWarehouse/GtoolsWarehouse.svelte";
-  import GuiTitle from "$lib/GuiComponents/GuiTitle/GuiTitle.svelte";
   import { onMount } from "svelte";
-  import { scale, slide } from "svelte/transition";
 
-  import GtoolsStore from "../../../database/Gtools/GtoolsStore";
-
-  $: isAtLeastOneChecked = $GtoolsStore.some((obj) => obj.isEditChecked);
-
+  import GtoolsWarehouse from "$lib/Gcore/Gtools/GtoolsMagazzino/GtoolsMagazzino.svelte";
+  import GuiToolsMagazzinoNavbar from "$lib/Gcore/Gtools/GtoolsMagazzino/GuiToolsMagazzino/GuiToolsMagazzinoNavbar/GuiToolsMagazzinoNavbar.svelte";
+  import GuiTitle from "$lib/GuiComponents/GuiTitle/GuiTitle.svelte";
+  
   let GtoolsPageContainer: HTMLElement;
-
+  
   onMount(() => {
     GtoolsPageContainer!.parentElement!.style.padding = "0px";
   });
 </script>
 
-<div class="flex flex-col h-full" bind:this={GtoolsPageContainer}>
+<div class="flex flex-col h-screen transition" bind:this={GtoolsPageContainer}>
   <div class="overflow-auto p-4">
     <GuiTitle Gtitle="tools" />
     <GtoolsWarehouse />
   </div>
 
-  {#if isAtLeastOneChecked}
-    <div class="flex bg-white w-full p-4" transition:scale>
-      <button
-        transition:slide
-        class="flex-1 bg-red-500 shadow-lg shadow-red-500 rounded-lg p-4 text-white font-semibold uppercase"
-        >delete</button
-      >
-    </div>
-  {/if}
+  <GuiToolsMagazzinoNavbar />
 </div>
