a37bf4c272de4e19f60762510b5c60828436cd3c
laaouatni <laaouatni.anas@outlook.it>
2023-04-07 16:26:35 +0200

    added clickOutside and click ESC in GuiPopup

--- gcnc/src/lib/GuiComponents/GuiPopup/GuiPopup.svelte
+++ gcnc/src/lib/GuiComponents/GuiPopup/GuiPopup.svelte
@@ -4,12 +4,28 @@
   import { fade } from "svelte/transition";
 
   export let GisPopupOpen: boolean;
+
+  function handleClose() {
+    GisPopupOpen = false;
+  }
+
+  function handleClick() {
+    handleClose();
+  }
+
+  function handleKeyUp(e: KeyboardEvent) {
+    if(!GisPopupOpen) return;
+    if (e.key === "Escape") handleClose();
+  }
 </script>
+
+<svelte:body on:keyup={handleKeyUp} />
 
 {#if GisPopupOpen}
   <div
     transition:fade
     class="absolute w-full h-screen overflow-auto outline grid place-items-center backdrop-blur-sm bg-black/50 p-4"
+    on:click|self={handleClick}
   >
     <GuiPopupDialog bind:GisPopupOpen></GuiPopupDialog>
   </div>
